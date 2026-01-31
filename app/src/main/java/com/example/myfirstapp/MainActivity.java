c675763b2f1addae31e755d63c6ad5c5efbc9d0f
DanielQuintanillaPaniagua <danielquintanillapaniagua@gmail.com>
2026-01-30 18:05:35 -0600

    se agrego la funcion de sumar  2 numeros

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,19 +1,34 @@
 package com.example.myfirstapp;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView tempVal;
+
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btnCalcular);
+        btn.setOnClickListener(v->calcular());
+    }
+    private void calcular(){
+        tempVal = findViewById(R.id.txtNum1);
+        Double num1 = Double.parseDouble(tempVal.getText().toString());
+
+        tempVal = findViewById(R.id.textNume2);
+        Double num2 = Double.parseDouble(tempVal.getText().toString());
+
+        double respuesta = num1 + num2;
+
+        tempVal = findViewById(R.id.lblRespuesta);
+        tempVal.setText("Respuesta: "+ respuesta);
 
     }
 }
