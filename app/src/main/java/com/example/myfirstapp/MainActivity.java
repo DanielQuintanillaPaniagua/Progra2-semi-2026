e7adae8007af857d908357a43e69720cbb516faa
DanielQuintanillaPaniagua <danielquintanillapaniagua@gmail.com>
2026-02-18 19:09:12 -0600

    correcion de el bug de los array

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -16,24 +16,23 @@ public class MainActivity extends AppCompatActivity {
     Spinner spn;
     Button btn;
     Double valores[][] = {
-            {1.0, 0.92, 7.79, 24.70, 36.80, 528.50, 17.10, 0.79, 149.50, 1.39, 0.88, 7.24}, //moendas
-            {1.0, 1000.0, 0.01, 0.001, 1609.34, 0.0254, 0.3048}, //longitud
-            {1.0, 0.001, 3.78541}, //volumen
-            { 1.0, 0.001, 0.453592, 0.0283495, 1000.0},//masa
-            {1.0, 1024.0, 1048576.0, 1073741824.0, 1099511627776.0, 0.125} ,//almacenamiento
-            {1.0, 60.0, 3600.0, 86400.0, 604800.0, 2592000.0, 31536000.0, 0.001},//tiempo
-            {1.0, 1000.0, 1000000.0, 1000000000.0, 8.0, 8000.0, 8000000.0 } //transferencia
+            {1.0, 0.92, 7.79, 24.70, 36.80, 528.50, 17.10, 0.79, 149.50, 1.39, 0.88, 7.24},//moendas
+            {1.0, 0.001, 100.0, 1000.0, 0.000621371, 39.3701, 3.28084},//longitud
+            {1.0, 1000.0, 0.264172},//volumen
+            {1.0, 1000.0, 2.20462, 35.274, 0.001},//masa
+            {1.0, 1024.0, 1048576.0, 1073741824.0, 1099511627776.0, 0.125},//almacenamiento
+            {1.0, 60.0, 3600.0, 86400.0, 604800.0, 2592000.0, 31536000.0, 0.001}, //tiempo
+            {1.0, 1000.0, 1000000.0, 1000000000.0, 8.0, 8000.0, 8000000.0}//transferencia
     };
     String[][] etiquetas = {
-            {"Dolar", "Euro", "Quetzal", "Lempira", "Cordoba", "Colon CR"," Peso Mexicano"," Libra Esterlina","Yen Japonés "," Dólar Canadiense","Franco Suizo","Yuan Chino "}, //monedas
-            {"Metro","Kilómetro","Centímetro","Milímetro","Milla","Pulgada","Pie"}, //Longitud
-            {"Litro","Mililitro","Galón"},  //volumen
-            {"Kilogramo","Gramo","Libra","Onza","Tonelada"}, //masa
-            {"Byte","Kilobyte ","Megabyte","Gigabyte","Terabyte","Bit"},//almacenamiento
-            {"Segundo","Minuto","Hora","Día","Semana","Mes","Año","Milisegundo"}, //tiempo
-            {"bps","Kbps","Mbps","Gbps","Bps","KBps ","MBps "}//transferiencia
+            {"Dolar", "Euro", "Quetzal", "Lempira", "Cordoba", "Colon CR", " Peso Mexicano", " Libra Esterlina", "Yen Japonés ", " Dólar Canadiense", "Franco Suizo", "Yuan Chino "}, //monedas
+            {"Metro", "Kilómetro", "Centímetro", "Milímetro", "Milla", "Pulgada", "Pie"}, //Longitud
+            {"Litro", "Mililitro", "Galón"},  //volumen
+            {"Kilogramo", "Gramo", "Libra", "Onza", "Tonelada"}, //masa
+            {"Byte", "Kilobyte ", "Megabyte", "Gigabyte", "Terabyte", "Bit"},//almacenamiento
+            {"Segundo", "Minuto", "Hora", "Día", "Semana", "Mes", "Año", "Milisegundo"}, //tiempo
+            {"bps", "Kbps", "Mbps", "Gbps", "Bps", "KBps ", "MBps "}//transferiencia
     };
-
 
 
     @Override
@@ -42,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn = findViewById(R.id.btnConvertir);
-        btn.setOnClickListener(v->convertir());
+        btn.setOnClickListener(v -> convertir());
 
         cambiarEtiqueta(0);//valores predeterminaods
 
@@ -52,13 +51,15 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 cambiarEtiqueta(i);
             }
+
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
 
             }
         });
     }
-    private void cambiarEtiqueta(int posicion){
+
+    private void cambiarEtiqueta(int posicion) {
         ArrayAdapter<String> aaEtiquetas = new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_spinner_item,
@@ -71,7 +72,8 @@ public class MainActivity extends AppCompatActivity {
         spn = findViewById(R.id.spnA);
         spn.setAdapter(aaEtiquetas);
     }
-    private void convertir(){
+
+    private void convertir() {
         spn = findViewById(R.id.spnTipo);
         int tipo = spn.getSelectedItemPosition();
 
@@ -86,9 +88,12 @@ public class MainActivity extends AppCompatActivity {
         double respuesta = conversor(tipo, de, a, cantidad);
 
         tempVal = findViewById(R.id.lblRespuesta);
-        tempVal.setText("Respuesta: "+ respuesta);
+        tempVal.setText("Respuesta: " + respuesta);
     }
-    double conversor(int tipo, int de, int a, double cantidad){
-        return valores[tipo][de]/valores[tipo][a] * cantidad;
+
+    double conversor(int tipo, int de, int a, double cantidad) {
+        return cantidad * (valores[tipo][a] / valores[tipo][de]);
     }
-    }
+}
+
+
