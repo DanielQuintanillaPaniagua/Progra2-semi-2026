accfcda04304d19e594c60be947d46f8e8ab301d
DanielQuintanillaPaniagua <danielquintanillapaniagua@gmail.com>
2026-05-11 18:17:21 -0600

    inicio del proyecto mensajeria

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,82 +1,29 @@
 package com.example.myfirstapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Spinner;
+import android.widget.ImageView;
 import android.widget.TextView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DatabaseReference;
 
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
-
-    TextView tempVal;
-
+public class MainActivity extends Activity {
+    FloatingActionButton fab;
     Button btn;
-    Spinner spn;
+    TextView tempVal;
+    String accion = "", idAmigo = "", id = "", rev = "";
+    ImageView img;
+    String urlCompletaFoto = "", getUrlCompletaFotoFireStore = "";
+    Intent tomarFotoIntent;
+    detectarinternet di;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = findViewById(R.id.btnCalcular);
-        btn.setOnClickListener(v -> calcular());
+
     }
-
-    private void calcular() {
-        tempVal = findViewById(R.id.txtNum1);
-        Double num1 = Double.parseDouble(tempVal.getText().toString());
-
-        tempVal = findViewById(R.id.textNume2);
-        Double num2 = Double.parseDouble(tempVal.getText().toString());
-        double respuesta = 0;
-        spn = findViewById(R.id.cboOpciones);
-        switch (spn.getSelectedItemPosition()) {
-            case 0: //suma
-                respuesta = num1 + num2;
-                break;
-            case 1: //Resta
-                respuesta = num1 - num2;
-                break;
-            case 2: //Multiplicacion
-                respuesta = num1 * num2;
-                break;
-            case 3: //division
-                respuesta = num1 / num2;
-                break;
-            case 4: //factorial
-                respuesta = calcularFactorial(num1.intValue());
-                break;
-            case 5: //prosentaje
-                respuesta = num1 * num2 / 100;
-                break;
-            case 6://exponente
-                respuesta = Math.pow(num1, num2);
-                break;
-            case 7://raiz
-                respuesta = Math.sqrt(num1);
-        }
-
-                tempVal = findViewById(R.id.lblRespuesta);
-        tempVal.setText("Respuesta: " + respuesta);
-    }
-        private long calcularFactorial(int n) {
-            if (n < 0) {
-                return 0;
-            }
-            if (n == 0 || n == 1) {
-                return 1;
-            }
-
-            long factorial = 1;
-            for (int i = 2; i <= n; i++) {
-                factorial *= i;
-            }
-            return factorial;
-
-
-
-}
 }
